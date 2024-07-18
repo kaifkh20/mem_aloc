415f175c0e65417150e10b87ca27189a1fe2c4f2
kaifkh20 <kaifkhan.biz@gmail.com>
2024-07-18 22:48:49 +0530

    Requesting mem using sbrk, no free list implementation yet

--- alloc_c.c
+++ alloc_c.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -15,7 +16,7 @@ typedef struct Block{
 }Block;
 
 Block* heapStart = NULL;
-Block* top;
+Block* top = NULL;
 
 // using bitmask to align
 static inline size_t align_mem(size_t n){
@@ -26,11 +27,6 @@ static inline size_t allocSize(size_t size){
   return size + sizeof(Block) - sizeof(((Block*)0)->data);
 }
 
-word_t* alloc_mem(size_t size){
-  size = align_mem(size);
-  printf("this is size %ld\n",size);
-}
-
 Block* req_from_os(size_t size){
   Block* block = (Block*)sbrk(0);
   if(sbrk(allocSize(size))==(void*)-1){
@@ -38,9 +34,49 @@ Block* req_from_os(size_t size){
   }
   return block;
 }
+word_t* alloc_mem(size_t size){
+  size = align_mem(size);
+
+  Block* block = req_from_os(size);
+  block->size = size;
+  block->used = true;
+
+  if(heapStart==NULL){
+    heapStart = block; 
+  }
+
+  if(top!=NULL){
+    top->next = block;
+  }
+
+  top = block;
+
+  return block->data;
+
+  // printf("this is size %ld\n",size);
+}
+
+Block* getHeader(word_t *data){
+  return (Block*)((char*)data + sizeof(((Block*)0)->data)-sizeof(Block));
+}
+
 
 int main(){
-  word_t* word = alloc_mem(5);
-  printf("hello world\n");
+  // word_t* word = alloc_mem(5);
+  // printf("hello world\n");
+
+  // Aligned to 8 bytes 
+
+  word_t* p1 = alloc_mem(3);
+  Block* p1b = getHeader(p1);
+  assert(p1b->size==sizeof(word_t));
+
+  word_t* p2 = alloc_mem(8);
+  Block* p2b = getHeader(p2);
+  assert(p2b->size==8);
+
+  printf("\nAll Assertions Passed\n");
+
+
   return 0;
 }
